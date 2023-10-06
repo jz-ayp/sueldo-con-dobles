9d2a7b9a540b7a308f7f7fd333affd21b7f60cc2
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2023-10-05 22:59:28 -0600

    Add solution for testing

---
+++ sueldo.bak
@@ -0,0 +1,16 @@
+"""
+Inserta el encabezado aquí y escribe tu código abajo
+"""
+
+
+# Declaraciones
+CONSTANTE = valor
+
+# Entradas
+entrada = input()
+
+# Proceso
+
+
+# Salidas
+print(salida)
--- sueldo.py
+++ sueldo.py
@@ -1,15 +1,26 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
-
-
-# Declaraciones
-CONSTANTE = valor
+Calcular sueldo semanal, incluyendo pago por horas dobles
+"""
 
 # Entradas
-entrada = input()
-
+horas = eval(input("Introduzca las horas trabajadas: "))
+tarifa = eval(input("Introduzca la tarifa horaria: "))
+		
 # Proceso
+if horas <= 48:
+    # Puras horas sencillas
+	sueldo1 = horas * tarifa
+	horas2 = 0
+	sueldo2 = 0
+else:
+	sueldo1 = 48 * tarifa
+	horas2 = horas - 48
+	sueldo2 = horas2 * tarifa * 2
 
-
+sueldoT = sueldo1 + sueldo2
+	
 # Salidas
-print(salida)
+
+print("Horas extras:  ", horas2)
+print("Sueldo extra:  ", sueldo2)
+print("Sueldo total:  ", sueldoT)
