06867068ee215b88c0ded5e6571891f6dba3e18b
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2023-10-05 23:04:20 -0600

    Remove solution

--- sueldo.py
+++ sueldo.py
@@ -1,26 +1,16 @@
 """
-Calcular sueldo semanal, incluyendo pago por horas dobles
+Inserta el encabezado aquí y escribe tu código abajo
 """
 
+
+# Declaraciones
+CONSTANTE = valor
+
 # Entradas
-horas = eval(input("Introduzca las horas trabajadas: "))
-tarifa = eval(input("Introduzca la tarifa horaria: "))
-		
+entrada = input()
+
 # Proceso
-if horas <= 48:
-    # Puras horas sencillas
-	sueldo1 = horas * tarifa
-	horas2 = 0
-	sueldo2 = 0
-else:
-	sueldo1 = 48 * tarifa
-	horas2 = horas - 48
-	sueldo2 = horas2 * tarifa * 2
 
-sueldoT = sueldo1 + sueldo2
-	
+
 # Salidas
-
-print("Horas extras:  ", horas2)
-print("Sueldo extra:  ", sueldo2)
-print("Sueldo total:  ", sueldoT)
+print(salida)
